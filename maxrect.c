5193738352a720e43c9549a141cb4c751f732b3d
David Wang <millimillenary@gmail.com>
2016-08-07 16:09:12 +1000

    Add approximate, generate-and-test three contact point case solution.

--- maxrect.c
+++ maxrect.c
@@ -6,11 +6,30 @@
 #include <math.h>
 #include <string.h>
 
+enum {
+	NUM_EDGES_IN_QUAD = NUM_VERTICES_IN_QUAD
+};
+
 static bool
 is_point_inside_quad(const Quad *quad, const Point *p);
 
 static Rect
 two_contact_case(const Quad *quad, int v);
+
+static Rect
+three_contact_case(const Quad *quad, int i, int j);
+
+static bool
+find_same_x_edge_segment(
+	const Point *p, const Point *q,
+	const Point *a, const Point *b,
+	Point *out_a, Point *out_b);
+
+static bool
+find_same_y_edge_segment(
+	const Point *p, const Point *q,
+	const Point *a, const Point *b,
+	Point *out_a, Point *out_b);
 
 /*--------------------------------------------------------------*/
 
@@ -78,6 +97,22 @@ max_aarect_in_quad(const Quad *quad)
 		if (area > best_area) {
 			best_rect = rect;
 			best_area = area;
+		}
+	}
+
+	/* Three contact point cases. */
+	for (int i = 0; i < NUM_EDGES_IN_QUAD; i++) {
+		for (int j = 0; j < NUM_EDGES_IN_QUAD; j++) {
+			if (j == i)
+				continue;
+
+			const Rect rect = three_contact_case(quad, i, j);
+			const double area = fabs((rect.x2 - rect.x1) * (rect.y2 - rect.y1));
+
+			if (area > best_area) {
+				best_rect = rect;
+				best_area = area;
+			}
 		}
 	}
 
@@ -177,4 +212,151 @@ two_contact_case(const Quad *quad, int contact)
 	}
 
 	return best_rect;
+}
+
+static Rect
+three_contact_case(const Quad *quad, int i, int j)
+{
+	Rect best_rect;
+	double best_area = 0.0;
+
+	const Point * const p = &(quad->p[i]);
+	const Point * const q = &(quad->p[next_vertex_index(i)]);
+	Point xprime1, xprime2;
+	double s_lower_x = 0.0;
+	double s_upper_x = 1.0;
+
+	memset(&best_rect, 0, sizeof(best_rect));
+
+	/* Find equation for x'(s). */
+	if (!find_same_y_edge_segment(
+			p, q, &(quad->p[j]), &(quad->p[next_vertex_index(j)]),
+			&xprime1, &xprime2)) {
+		return best_rect;
+	}
+
+	if (fabs(q->y - p->y) > 0.0) {
+		const Point * const a = &(quad->p[j]);
+		const Point * const b = &(quad->p[next_vertex_index(j)]);
+		const double s1 = (a->y - p->y) / (q->y - p->y);
+		const double s2 = (b->y - p->y) / (q->y - p->y);
+
+		s_lower_x = max(s_lower_x, min(s1, s2));
+		s_upper_x = min(s_upper_x, max(s1, s2));
+	}
+
+	/* Take 100 sample points along the edge.
+	 * For each x(s) and x'(s), find a third contact point at y'(s).
+	 */
+	for (int iter = 0; iter <= 100; iter++) {
+		const double s = lerp(s_lower_x, s_upper_x, (double)iter / 100.0);
+
+		const double x = lerp(p->x, q->x, s);
+		const double y = lerp(p->y, q->y, s);
+		const double xprime = lerp(xprime1.x, xprime2.x, s);
+
+		for (int k = 0; k < NUM_EDGES_IN_QUAD; k++) {
+			for (int yy = 0; yy < 2; yy++) {
+				Point yprime1, yprime2;
+
+				if (yy == 0) {
+					if (!find_same_x_edge_segment(
+							p, q, &(quad->p[k]), &(quad->p[next_vertex_index(k)]),
+							&yprime1, &yprime2)) {
+						continue;
+					}
+				} else {
+					if (!find_same_x_edge_segment(
+							&xprime1, &xprime2, &(quad->p[k]), &(quad->p[next_vertex_index(k)]),
+							&yprime1, &yprime2)) {
+						continue;
+					}
+				}
+
+				const double yprime = lerp(yprime1.y, yprime2.y, s);
+
+				const double area = fabs((xprime - x) * (yprime - y));
+				const Point test1 = { x, yprime };
+				const Point test2 = { xprime, yprime };
+
+				if (area > best_area
+						&& is_point_inside_quad(quad, &test1)
+						&& is_point_inside_quad(quad, &test2)) {
+					best_rect.x1 = min(x, xprime);
+					best_rect.y1 = min(y, yprime);
+					best_rect.x2 = max(x, xprime);
+					best_rect.y2 = max(y, yprime);
+					best_area = area;
+				}
+			}
+		}
+	}
+
+	return best_rect;
+}
+
+static bool
+find_same_x_edge_segment(
+	const Point *p, const Point *q,
+	const Point *a, const Point *b,
+	Point *out_a, Point *out_b)
+{
+	if (fabs(b->x - a->x) > 0.0) {
+		const double c = (p->x - a->x) / (b->x - a->x);
+		const double m = (q->x - p->x) / (b->x - a->x);
+
+		out_a->x = p->x;
+		out_a->y = a->y + (b->y - a->y) * c;
+		out_b->x = q->x;
+		out_b->y = (b->y - a->y) * m + out_a->y;
+
+		/* Check: out_a->x == p->x, out_b->x == q->x. */
+		/*
+		const double epsilon = 0.1;
+		out_a->x = a->x + (b->x - a->x) * c;
+		out_b->x = (b->x - a->x) * m + out_a->x;
+		assert(fabs(p->x - out_a->x) < epsilon);
+		assert(fabs(q->x - out_b->x) < epsilon);
+		*/
+		return true;
+	} else {
+		out_a->x = 0.0;
+		out_a->y = 0.0;
+		out_b->x = 0.0;
+		out_b->y = 0.0;
+		return false;
+	}
+}
+
+static bool
+find_same_y_edge_segment(
+	const Point *p, const Point *q,
+	const Point *a, const Point *b,
+	Point *out_a, Point *out_b)
+{
+	if (fabs(b->y - a->y) > 0.0) {
+		const double c = (p->y - a->y) / (b->y - a->y);
+		const double m = (q->y - p->y) / (b->y - a->y);
+
+		out_a->x = a->x + (b->x - a->x) * c;
+		out_a->y = p->y;
+		out_b->x = (b->x - a->x) * m + out_a->x;
+		out_b->y = q->y;
+
+		/* Check: out_a->y == p->y, out_b->y == q->y. */
+		/*
+		const double epsilon = 0.1;
+		out_a->y = a->y + (b->y - a->y) * c;
+		out_b->y = (b->y - a->y) * m + out_a->y;
+		assert(fabs(p->y - out_a->y) < epsilon);
+		assert(fabs(q->y - out_b->y) < epsilon);
+		*/
+		return true;
+	} else {
+		out_a->x = 0.0;
+		out_a->y = 0.0;
+		out_b->x = 0.0;
+		out_b->y = 0.0;
+		return false;
+	}
 }
