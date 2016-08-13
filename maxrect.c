cbba94f02fa601b0aad69e6074e8d5d2aa333857
David Wang <millimillenary@gmail.com>
2016-08-13 10:10:42 +1000

    Improve three point contact solver.

--- maxrect.c
+++ maxrect.c
@@ -6,6 +6,8 @@
 #include <math.h>
 #include <string.h>
 
+/*#define BRUTE_FORCE_CHECK*/
+
 enum {
 	NUM_EDGES_IN_QUAD = NUM_VERTICES_IN_QUAD
 };
@@ -17,7 +19,16 @@ static Rect
 two_contact_case(const Quad *quad, int v);
 
 static Rect
-three_contact_case(const Quad *quad, int i, int j);
+three_contact_case(const Quad *quad, int i, int j, int k);
+
+#ifdef BRUTE_FORCE_CHECK
+#include <stdio.h>
+static Rect
+three_contact_case_check(const Quad *quad, int i, int j);
+#endif
+
+static int
+find_last_edge(int i, int j, int k);
 
 static bool
 find_same_x_edge_segment(
@@ -106,15 +117,44 @@ max_aarect_in_quad(const Quad *quad)
 			if (j == i)
 				continue;
 
-			const Rect rect = three_contact_case(quad, i, j);
-			const double area = fabs((rect.x2 - rect.x1) * (rect.y2 - rect.y1));
+			for (int k = 0; k < NUM_EDGES_IN_QUAD; k++) {
+				if (k == i || k == j)
+					continue;
 
-			if (area > best_area) {
+				const Rect rect = three_contact_case(quad, i, j, k);
+				const double area = fabs((rect.x2 - rect.x1) * (rect.y2 - rect.y1));
+
+				if (area > best_area) {
+					best_rect = rect;
+					best_area = area;
+				}
+			}
+		}
+	}
+
+#ifdef BRUTE_FORCE_CHECK
+	/* Brute force check. */
+	for (int i = 0; i < NUM_EDGES_IN_QUAD; i++) {
+		for (int j = 0; j < NUM_EDGES_IN_QUAD; j++) {
+			if (j == i)
+				continue;
+
+			const Rect rect = three_contact_case_check(quad, i, j);
+			const double area = fabs((rect.x2 - rect.x1) * (rect.y2 - rect.y1));
+			const double epsilon = 0.1;
+
+			if (area > best_area + epsilon) {
+				printf("Brute force check found better area: %g vs %g\n", area, best_area);
+				printf("p[0] x=%g, y=%g\n", quad->p[0].x, quad->p[0].y);
+				printf("p[1] x=%g, y=%g\n", quad->p[1].x, quad->p[1].y);
+				printf("p[2] x=%g, y=%g\n", quad->p[2].x, quad->p[2].y);
+				printf("p[3] x=%g, y=%g\n", quad->p[3].x, quad->p[3].y);
 				best_rect = rect;
 				best_area = area;
 			}
 		}
 	}
+#endif
 
 	return best_rect;
 }
@@ -215,7 +255,130 @@ two_contact_case(const Quad *quad, int contact)
 }
 
 static Rect
-three_contact_case(const Quad *quad, int i, int j)
+three_contact_case(const Quad *quad, int i, int j, int k)
+{
+	Rect best_rect;
+	double best_area = 0.0;
+
+	const Point * const p = &(quad->p[i]);
+	const Point * const q = &(quad->p[next_vertex_index(i)]);
+	const int l = find_last_edge(i, j, k);
+
+	Point xprime1, xprime2;
+	Point yprime1, yprime2;
+	Point yy1, yy2;
+	double s_lower = 0.0;
+	double s_upper = 1.0;
+	double s_intersection;
+
+	memset(&best_rect, 0, sizeof(best_rect));
+
+	/* Find equation for x'(s). */
+	if (!find_same_y_edge_segment(
+			p, q, &(quad->p[j]), &(quad->p[next_vertex_index(j)]),
+			&xprime1, &xprime2)) {
+		return best_rect;
+	}
+
+	if (fabs(q->y - p->y) > 0.0) {
+		const Point * const a = &(quad->p[j]);
+		const Point * const b = &(quad->p[next_vertex_index(j)]);
+		const double s1 = (a->y - p->y) / (q->y - p->y);
+		const double s2 = (b->y - p->y) / (q->y - p->y);
+
+		s_lower = max(s_lower, min(s1, s2));
+		s_upper = min(s_upper, max(s1, s2));
+	}
+
+	if (s_upper < s_lower) {
+		return best_rect;
+	}
+
+	/* Find equation for y'(s). */
+	if (!find_same_x_edge_segment(
+			p, q, &(quad->p[k]), &(quad->p[next_vertex_index(k)]),
+			&yprime1, &yprime2)) {
+		return best_rect;
+	}
+
+	if (fabs(q->x - p->x) > 0.0) {
+		const Point * const a = &(quad->p[k]);
+		const Point * const b = &(quad->p[next_vertex_index(k)]);
+		const double s1 = (a->x - p->x) / (q->x - p->x);
+		const double s2 = (b->x - p->x) / (q->x - p->x);
+
+		s_lower = max(s_lower, min(s1, s2));
+		s_upper = min(s_upper, max(s1, s2));
+	}
+
+	if (s_upper < s_lower) {
+		return best_rect;
+	}
+
+	/* Find equation for yy(s) for the other potential contact point (x'(s), yy(s)).
+	 * If it exists, find the intersection y'(s) = yy(s).
+	 */
+	if (find_same_x_edge_segment(
+			&xprime1, &xprime2, &(quad->p[l]), &(quad->p[next_vertex_index(l)]),
+			&yy1, &yy2)) {
+		s_intersection = (yy1.y - yprime1.y) / ((yprime2.y - yprime1.y) - (yy2.y - yy1.y));
+		s_intersection = clamp(s_lower, s_intersection, s_upper);
+	} else {
+		s_intersection = s_upper;
+	}
+
+	/* Process in two parts:
+	 * iter 0: [s_lower, s_intersection], and
+	 * iter 1: [s_intersection, s_upper].
+	 */
+	for (int iter = 0; iter < 2; iter++) {
+		const double cx = p->x;
+		const double mx = q->x - p->x;
+		const double cy = p->y;
+		const double my = q->y - p->y;
+		const double cxprime = xprime1.x;
+		const double mxprime = xprime2.x - xprime1.x;
+		const double cyprime = yprime1.y;
+		const double myprime = yprime2.y - yprime1.y;
+
+		const double a = (mxprime - mx) * (myprime - my);
+		const double b = (mxprime - mx) * (cyprime - cy) + (myprime - my) * (cxprime - cx);
+		const double c = (cxprime - cx) * (cyprime - cy);
+		(void)c;
+
+		if (fabs(a) > 0.0) {
+			const double s
+				= (iter == 0)
+				? clamp(s_lower, -b / (2.0 * a), s_intersection)
+				: clamp(s_intersection, -b / (2.0 * a), s_upper);
+
+			const double x = lerp(p->x, q->x, s);
+			const double y = lerp(p->y, q->y, s);
+			const double xprime = lerp(xprime1.x, xprime2.x, s);
+			const double yprime = lerp(yprime1.y, yprime2.y, s);
+
+			const double area = fabs((xprime - x) * (yprime - y));
+			const Point test1 = { x, yprime };
+			const Point test2 = { xprime, yprime };
+
+			if (area > best_area
+					&& is_point_inside_quad(quad, &test1)
+					&& is_point_inside_quad(quad, &test2)) {
+				best_rect.x1 = min(x, xprime);
+				best_rect.y1 = min(y, yprime);
+				best_rect.x2 = max(x, xprime);
+				best_rect.y2 = max(y, yprime);
+				best_area = area;
+			}
+		}
+	}
+
+	return best_rect;
+}
+
+#ifdef BRUTE_FORCE_CHECK
+static Rect
+three_contact_case_check(const Quad *quad, int i, int j)
 {
 	Rect best_rect;
 	double best_area = 0.0;
@@ -293,6 +456,19 @@ three_contact_case(const Quad *quad, int i, int j)
 	}
 
 	return best_rect;
+}
+#endif
+
+static int
+find_last_edge(int i, int j, int k)
+{
+	for (int l = 0; l < NUM_EDGES_IN_QUAD; l++) {
+		if (l != i && l != j && l != k)
+			return l;
+	}
+
+	assert(false);
+	return -1;
 }
 
 static bool
